ff81b8ce7cc6f3b9598ecc4a3b3ca08d0c64688e
Sai Sampath Kumar <balivadask2000@gmail.com>
2020-05-08 23:49:48 +0530

    fixed issue with homepage

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createWindow() {
         resizable: false
     })
 
-    win.loadFile('app/download/index.html');
+    win.loadFile('app/home/index.html');
 
     win.webContents.openDevTools();
 }
